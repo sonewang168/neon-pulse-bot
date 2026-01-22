1200e05bf0f134f1ec5f1e86aa2da9e73cc4eb0d
sonewang <anony17898@gmail.com>
2026-01-22 09:25:48 +0800

    V11 full features

--- app.py
+++ app.py
@@ -2066,11 +2066,11 @@ def api_modify_stand():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard_v11.html')
+    return render_template('dashboard.html')
 
 @app.route('/')
 def index():
-    return render_template('dashboard_v11.html')
+    return render_template('dashboard.html')
 
 @app.route('/manifest.json')
 def manifest():
