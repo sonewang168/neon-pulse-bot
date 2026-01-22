e80326dc4c12339634dc9ed61e2e09d42e75be34
sonewang <anony17898@gmail.com>
2026-01-22 09:15:05 +0800

    V11 full PWA with all features

--- app.py
+++ app.py
@@ -2066,11 +2066,11 @@ def api_modify_stand():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard_v11.html')
 
 @app.route('/')
 def index():
-    return render_template('dashboard.html')
+    return render_template('dashboard_v11.html')
 
 @app.route('/manifest.json')
 def manifest():
